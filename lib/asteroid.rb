1d8fd5824676512d7e8ac1ae97cc037ed53c6089
jmks <jason.schweier@gmail.com>
2016-02-19 13:44:44 -0500

    New Asteroids spawn more asteroids when collided with

--- lib/asteroid.rb
+++ lib/asteroid.rb
@@ -1,20 +1,36 @@
 #Encoding: UTF-8
 
 class Asteroid
-  attr_reader :shape
+  attr_reader :shape, :spawn
 
-  def initialize(animation, shape)
+  def initialize(animation, shape, spawn = 2)
     @animation = animation
     @shape = shape
+    @spawn = spawn
     @color = random_color
     @shape.body.p = CP::Vec2.new(rand * WIDTH, rand * HEIGHT) # position
     @shape.body.v = CP::Vec2.new(0.0, 0.0) # velocity
     @shape.body.a = 3 * Math::PI / 2.0 # angle in radians; faces towards top of screen
   end
 
+  def self.cp_shape
+    CP::Shape::Circle.new(CP::Body.new(0.0001, 0.0001), 25/2, CP::Vec2.new(0.0, 0.0)).tap do |s|
+      s.collision_type = :asteroid
+    end
+  end
+
   def draw
     img = @animation[Gosu::milliseconds / 100 % @animation.size]
     img.draw(@shape.body.p.x - img.width / 2.0, @shape.body.p.y - img.height / 2.0, ZOrder::Asteroids, 1, 1, @color, :add)
+  end
+
+  def score
+    10
+  end
+
+  def spawns
+    return [] if @spawn.zero?
+    (0...@spawn).map { self.class.new(@animation, self.class.cp_shape, @spawn - 1) }
   end
 
   private
--- spec/lib/asteroid_spec.rb
+++ spec/lib/asteroid_spec.rb
@@ -32,4 +32,38 @@ RSpec.describe Asteroid do
       end
     end
   end
+
+  describe "#score" do
+    subject { described_class.new(double, double.as_null_object) }
+    it "has a score of 10" do
+      expect(subject.score).to be 10
+    end
+  end
+
+  describe "#spawns" do
+    context "with no remaining spawns" do
+      subject { described_class.new(double, double.as_null_object, 0) }
+
+      it "returns empty" do
+        expect(subject.spawns).to be_empty
+      end
+    end
+
+    context "with 2 spawns remaining" do
+      subject { described_class.new(double, double.as_null_object, 2) }
+
+      it "returns 2 asteroids" do
+        expect(subject.spawns.length).to be 2
+        subject.spawns.each do |obj|
+          expect(obj).to be_an_instance_of described_class
+        end
+      end
+
+      describe "the spawned asteroids" do
+        it "have one less spawn than parent" do
+          expect(subject.spawns.first.spawn).to eql 1
+        end
+      end
+    end
+  end
 end
