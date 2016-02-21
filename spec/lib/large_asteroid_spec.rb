35c8c9333b90d8f960d94138986321dbb22952f5
jmks <jason.schweier@gmail.com>
2016-02-21 10:52:16 -0500

    Added Large asteroid

---
+++ spec/lib/large_asteroid_spec.rb
@@ -0,0 +1,39 @@
+require "large_asteroid"
+
+RSpec.describe LargeAsteroid do
+  describe ".new" do
+    it { is_expected.to be_an_instance_of LargeAsteroid }
+
+    it "has an image" do
+      expect(subject.image).to be_an_instance_of Gosu::Image
+    end
+
+    it "has a shape" do
+      expect(subject.shape).to be_a CP::Shape
+    end
+
+    it "collides as an asteroid" do
+      expect(subject.shape.collision_type).to be :asteroid
+    end
+
+    it "associates itself to its shape" do
+      expect(subject.shape.object).to be subject
+    end
+
+    it "has a body" do
+      expect(subject.body).to be_an_instance_of CP::Body
+    end
+  end
+
+  describe "#points" do
+    it "is worth 20 points" do
+      expect(subject.points).to be 20
+    end
+  end
+
+  describe "chunks" do
+    it "breaks into 2 additional asteroids" do
+      expect(subject.chunks.length).to be 2
+    end
+  end
+end
