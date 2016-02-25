995f963b721e42219e00a8610342f6075b814cc5
David Andrews <david@ryatta.com>
2016-02-25 14:26:31 -0500

    Starting point for EPIC 1

--- lib/player.rb
+++ lib/player.rb
@@ -3,117 +3,54 @@
 # Ruby Hack Night Asteroids by David Andrews and Jason Schweier, 2016
 
 require_relative 'body'
-require_relative 'shot'
 
 class Player < Body
   @@ship_image = Gosu::Image.new("media/ship.bmp")
   @@thrust_image = Gosu::Image.new("media/shipthrust.bmp")
   @@thrust_sample = Gosu::Sample.new("media/thrust.wav").play(1, 1, true)
   @@thrust_sample.pause
-  @@invulnerable_time = 2000 # ms
-
-  attr_reader :destroyed
 
   def initialize(dt)
     super default_shape
     @dt = dt
-    new_ship
+    #create a new_ship
   end
 
   def new_ship
-    @destroyed = false
-    self.position = dead_center
-    self.velocity = still
-    self.accelerate(false)
-    self.angle = facing_upward
-    @invulnerability_expires = Gosu.milliseconds + @@invulnerable_time
-  end
-
-  def destroyed!
-    @destroyed = true
-    self.accelerate(false)
-    self.class.random_boom_sound.play
-  end
-
-  def invulnerable?
-    @invulnerability_expires > Gosu.milliseconds
-  end
-
-  def reset_forces
-    @shape.body.reset_forces
+    #position = dead_center
+    #velocity = still
+    #angle = facing_upward
   end
 
   def apply_damping
     @shape.body.update_velocity(zero_gravity, 0.996, @dt)
   end
 
-  # Directly set the position of our Player
   def warp_to(position)
-    self.position = position
+    #position = position
   end
 
-  def starting_position
-    self.position = dead_center
+  def turn
   end
 
-  # Turn in the given direction
-  def turn(right, left, rate = 6.0)
-    if right && !left
-      self.spin = rate
-    elsif left && !right
-      self.spin = -rate
-    else
-      self.spin = 0.0
-    end
+  def shoot
   end
 
-  def location_of_gun
-    # the shot must start outside of my body, otherwise a collision will be registered and I'll die
-    position + self.class.radians_to_vec2(angle) * 18
-  end
-
-  def shoot(shooting)
-    if shooting
-      return if @shooting || Shot.shots_taken(self) > 3 # return early if we have already shot for this keypress
-      Shot.shoot(location_of_gun, angle, self)
-    end
-    @shooting = shooting
-  end
-
-  def hyperspace(hyperspacing)
-    if hyperspacing
-      return if @hyperspacing
-      self.position = self.class.random_position
-      self.velocity = still
-    end
-    @hyperspacing = hyperspacing
+  def hyperspace
   end
 
   # Apply forward force; Chipmunk will do the rest
   # Here we must convert the angle (facing) of the body into
   # forward momentum by creating a vector in the direction of the facing
   # and with a magnitude representing the force we want to apply
-  def accelerate(accelerating, force = 2000.0)
-    reset_forces # When a force or torque is set on a body, it is cumulative - start from zero
-    @accelerating = accelerating
-    if accelerating
-      @shape.body.apply_force((self.class.radians_to_vec2(angle) * force), zero_offset)
-      @@thrust_sample.resume unless @@thrust_sample.playing?
-    else
-      @@thrust_sample.pause if @@thrust_sample.playing?
-    end
+  def accelerate(force = 2000.0)
+    @shape.body.reset_forces # When a force or torque is set on a body, it is cumulative - start from zero
+    @shape.body.apply_force((self.class.radians_to_vec2(angle) * force), zero_offset)
   end
 
   def draw
-    # simulate blinking when invulnerable
-    blink_speed = 100
     white = 0xff_ffffff
-    black = 0x00_000000
-
-    color = invulnerable? ? blink(Gosu.milliseconds, on: white, off: black) : white
-    image = @accelerating ? blink(Gosu.milliseconds, on: @@thrust_image, off: @@ship_image) : @@ship_image
-
-    image.draw_rot(position.x, position.y, ZOrder::Player, angle.radians_to_gosu, 0.5, 0.5, 1, 1, color)
+    @ship_image.draw_rot(position.x, position.y, ZOrder::Player, angle.radians_to_gosu, 0.5, 0.5, 1, 1, white)
   end
 
 private
@@ -134,9 +71,5 @@ private
       s.collision_type = :ship
       s.object = self
     end
-  end
-
-  def blink(time, on:, off:, blink_speed: 100)
-    time.div(blink_speed).even? ? on : off
   end
 end
